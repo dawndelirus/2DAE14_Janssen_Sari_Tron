49139dea448b83167f50b4d0e60fb830073f1893
SariJanssen <sari.lg.janssen@gmail.com>
2022-04-30 19:47:48 +0200

    creating pch for burger time

--- BurgerTime/BurgerTime.cpp
+++ BurgerTime/BurgerTime.cpp
@@ -1,14 +1,4 @@
-#pragma once
-
-//#include <stdio.h>
-//#include <iostream> // std::cout
-//#include <sstream> // stringstream
-#include <memory> // smart pointers
-#include <vector>
-#include <string>
-
-//#define WIN32_LEAN_AND_MEAN
-//#include <windows.h>
+#include "BurgerTimePCH.h"
 
 #if _DEBUG
 // ReSharper disable once CppUnusedIncludeDirective
---
+++ BurgerTime/BurgerTimePCH.h
@@ -0,0 +1,11 @@
+#pragma once
+
+//#include <stdio.h>
+//#include <iostream> // std::cout
+//#include <sstream> // stringstream
+#include <memory> // smart pointers
+#include <vector>
+#include <string>
+
+//#define WIN32_LEAN_AND_MEAN
+//#include <windows.h>
